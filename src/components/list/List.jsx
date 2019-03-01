fcd067312a07deb79a29880ff6cb71a2ff1306d8
Ruben E. Acosta <eldragondera1@gmail.com>
2019-02-28 23:17:12 -0500

    proptypes

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -2,8 +2,7 @@ import React, { Component } from 'react'
 import { handleResponse } from '../../helpers'
 import { API_URL } from '../../config'
 import Loading from '../common/Loading'
-
-import './table.css'
+import Table from './Table'
 
 class List extends Component {
   constructor(props) {
@@ -39,48 +38,23 @@ class List extends Component {
   }
 
   render () {
+    const { loading, error, currencies } = this.state
+
     // render only loading component
-    if (this.state.loading) {
+    if (loading) {
       return <div className="loading-container"><Loading /></div>
     }
 
     // render only error message, if error occurred while fetching data
-    if (this.state.error) {
-      return <div className="error">{this.state.error}</div>
+    if (error) {
+      return <div className="error">{error}</div>
     }
 
     return (
-      <div className='table-container'>
-        <table className="Table">
-          <thead className="Table-head">
-          <tr>
-            <th>Crytocurrency</th>
-            <th>Price</th>
-            <th>Market Cap</th>
-            <th>24H Change</th>
-          </tr>
-          </thead>
-          <tbody className="Table-body">
-            {this.state.currencies.map((currency) => (
-              <tr key={currency.id}>
-                <td>
-                  <span className="Table-rank">{currency.rank}</span>
-                  {currency.name}
-                </td>
-                <td>
-                  <span className="Table-dollar">$ {currency.price}</span>
-                </td>
-                <td>
-                  <span className="Table-dollar">$ {currency.marketCap}</span>
-                </td>
-                <td>
-                  {this.renderChangePercent(currency.percentChange24h)}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <Table
+        renderChangePercent={this.renderChangePercent}
+        currencies={currencies}
+      />
     )
   }
 }
